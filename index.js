0b4ccab8fbfb36749917d6c32492f047fe3f3dbc
Haider007A <100359869+Haider007A@users.noreply.github.com>
2024-05-24 15:40:48 +0500

    Socket.io configured and working correctly

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 require("ejs");
+const http = require("http");
 const path = require("path");
 const passport = require("passport");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const User = require("./models/user");
 const flash = require("connect-flash");
+const { Server } = require("socket.io");
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 const mongoConnect = require("./mongoConnect");
@@ -17,6 +19,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  socket.on("move", ({ x, y }) => {
+    io.emit("move", { x, y });
+  });
+  socket.on("game", (game) => {
+    io.emit("game", game);
+  });
+});
 
 const store = new MongoDBStore({
   mongoUrl: process.env.MONGO_URI,
@@ -46,7 +60,6 @@ const LocalStrategy = require("passport-local").Strategy;
 passport.use(new LocalStrategy(User.authenticate()));
 
 app.use("/", require("./routes"));
-
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}. http://localhost:${PORT}/`);
 });
--- routes.js
+++ routes.js
@@ -83,7 +83,6 @@ router.get("/game", async (req, res) => {
     players: user._id,
     status: "ongoing",
   }).populate("players");
-  let playersCount = 0;
 
   if (!game) {
     const existingGameWithOnePlayer = await Game.findOne({
@@ -96,7 +95,6 @@ router.get("/game", async (req, res) => {
       existingGameWithOnePlayer.players.push(user._id);
       await existingGameWithOnePlayer.save();
       game = existingGameWithOnePlayer;
-      playersCount = 2;
     } else {
       // Create a new game with the current user
       game = new Game({
@@ -106,51 +104,9 @@ router.get("/game", async (req, res) => {
           .map(() => Array(10).fill(null)),
       });
       await game.save();
-      playersCount = 1;
     }
-  } else {
-    playersCount = game.players.length;
   }
-
-  res.render("game", { user, game, playersCount });
-});
-
-router.post("/place-monster", async (req, res) => {
-  if (!req.isAuthenticated())
-    return res.status(401).json({ message: "Unauthorized" });
-
-  const user = req.user;
-  const { x, y } = req.body;
-
-  try {
-    const game = await Game.findOne({ players: user._id, status: "ongoing" });
-    if (!game) return res.status(404).json({ message: "Game not found" });
-
-    // Check if it's the player's turn
-    if (game.turn.toString() !== user._id.toString()) {
-      return res.status(400).json({ message: "Not your turn" });
-    }
-
-    // Check if the cell is valid to place monster
-    const isValidCell = validateCell(game.grid, x, y);
-    if (!isValidCell) {
-      return res.status(400).json({ message: "Invalid cell" });
-    }
-
-    // Update the game grid with placed monster
-    game.grid[x][y] = user.username;
-
-    // Update turn and save game
-    const nextPlayerIndex =
-      (game.players.indexOf(user._id) + 1) % game.players.length;
-    game.turn = game.players[nextPlayerIndex];
-    await game.save();
-
-    res.status(200).json({ message: "Monster placed successfully", game });
-  } catch (error) {
-    console.error("Error placing monster:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
+  res.render("game", { user, game });
 });
 
 module.exports = router;
